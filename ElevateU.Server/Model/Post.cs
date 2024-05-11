fe9dadc98e160888adeeb87ff2256a040ddc83a9
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-11 17:13:29 +0300

    add post

---
+++ ElevateU.Server/Model/Post.cs
@@ -0,0 +1,7 @@
+public class Post{
+     public int PostID {get; set;}
+     public int UserID  {get; set;}
+     public string Content { get; set; }
+    public DateTime PostDate { get; set; }
+
+}
