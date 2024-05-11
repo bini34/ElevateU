caf9fece7638056b594836999a617030c57d681c
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-11 18:12:55 +0300

    update group class

---
+++ ElevateU.Server/Model/Group.cs
@@ -0,0 +1,8 @@
+
+public class Group{
+    public int GroupID  {get; set;}
+    public string GroupName  {get; set;}
+    public string Description  {get; set;}
+    public DateTime CreationDate  {get; set;}
+
+}
