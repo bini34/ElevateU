e4eaf7e6c9d14dff738baf8db204d7e60f1fc889
Biniyam Ambachew <biniyamambachew@gmail.com>
2024-05-11 17:13:17 +0300

    add media

---
+++ ElevateU.Server/Model/Media.cs
@@ -0,0 +1,9 @@
+class Media{
+    public int MediaID {get; set;}
+    public string MediaType { get; set; }
+    public byte[] MediaData { get; set; }
+
+    public int PostID {get; set;}
+    public Post Post { get; set; }
+
+}
